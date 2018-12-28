c77dcb35f7e5ac590a014977505cdf36b21310f1
scorpionipx <scorpionipx@gmail.com>
2018-12-28 15:06:38 +0200

    base project

---
+++ control_loop/AVR/Atmega32A/PIDvsFuzzy/PIDvsFuzzy/main.c
@@ -0,0 +1,18 @@
+/*
+ * PIDvsFuzzy.c
+ *
+ * Created: 28-Dec-18 15:05:26
+ * Author : uidq6025
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
