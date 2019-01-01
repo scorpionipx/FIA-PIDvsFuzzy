cca3920bb3102540a47958cb4bebe67bc71fef5c
scorpionipx <scorpionipx@gmail.com>
2019-01-01 17:59:28 +0200

    Allow Atmel Studio project files generation

---
+++ control_loop/AVR/Atmega32A/PIDvsFuzzy/PIDvsFuzzy/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc_driver.c
+
+control_loop.c
+
+display_driver.c
+
+fuzzy.c
+
+main.c
+
+optocoupler_driver.c
+
+pid.c
+
+pwm_driver.c
+
+usart_driver.c
+
